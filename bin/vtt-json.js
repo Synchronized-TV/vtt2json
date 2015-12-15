add4a6a0cf26a34b04f5ad0aa8fff2380ba16987
Julien De Luca <jide.fr@gmail.com>
2015-12-15 16:11:55 +0100

    Make possible to pass a directory as input

--- bin/vtt-json.js
+++ bin/vtt-json.js
@@ -3,9 +3,42 @@ var fs = require('fs');
 var minimist = require('minimist');
 var through2 = require('through2');
 var stringify = require('json-stable-stringify');
+var async = require('async');
+var path = require('path');
+var Stream = require('stream');
 
 var vtt = require('./../');
 var generateCue = require('../lib/generateCue');
+
+function getConcatenatedReadStream(directory, onStream) {
+    if (fs.lstatSync(directory).isFile()) {
+        onStream(fs.createReadStream(directory));
+    }
+    else {
+        fs.readdir(directory, function (err, files) {
+          var concatenated = '';
+          async.eachSeries(files, function (file, callback) {
+            if (file.indexOf('.vtt') === -1) { return callback(); }
+
+            var currentFile = path.join(directory, file);
+            fs.stat(currentFile, function (err, stats) {
+              if (stats.isDirectory()) { return callback(); }
+
+              var stream = fs.createReadStream(currentFile).on('end', function () {
+                callback();
+              }).on('data', function (data) { concatenated += data.toString('utf8'); });
+            });
+          }, function() {
+            var s = new Stream.Readable();
+            s._read = function noop() {};
+            s.push(concatenated);
+            s.push(null);
+
+            onStream(s);
+          })
+        });
+    }
+}
 
 var argv = minimist(process.argv, {
     alias: {
@@ -26,26 +59,22 @@ var output = (argv.output && argv.output !== '-') ? fs.createWriteStream(argv.ou
 
 var input;
 if (filename === '-' || !filename) input = process.stdin;
-else if (fs.existsSync(filename)) input = fs.createReadStream(filename);
-else {
-    console.error('File: %s does not exist', filename);
-    process.exit(2);
-}
 
 if (argv._[2] === 'parse') {
-    var stream = input.pipe(vtt.parse())
+    var stream = getConcatenatedReadStream(filename, input => {
+        input.pipe(vtt.parse())
         .pipe(through2.obj(function(obj, enc, cb) {
             cb(null, generateCue(obj))
         }))
         .pipe(through2.obj(function(obj, enc, cb) {
-            //console.log(JSON.stringify(obj, null, ' ').indexOf('testr') > -1);
             //cb(null, JSON.stringify(obj, null, ' '))
             cb(null, stringify(obj, function (a, b) {
                 // Important: sort by time (keys)
                 return parseFloat(a.key) - parseFloat(b.key);
             }, { space: '  ' }))
         }))
-        .pipe(output);
+        .pipe(output)
+    });
 }
 
 if (argv._[2] === 'serialize') {
